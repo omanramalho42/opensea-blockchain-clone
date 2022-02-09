ddf23105a34e6fdd53123c890d4baa7ebcf81ee1
Oman Ramalho <omanapple42@hotmail.com>
2022-02-09 19:17:57 -0300

    first commit

---
+++ pages/nfts/[nftId].js
@@ -0,0 +1,90 @@
+import Header from '../../components/Header'
+import { useEffect, useMemo, useState } from 'react'
+import { useWeb3 } from '@3rdweb/hooks'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import { useRouter } from 'next/router'
+import NFTImage from '../../components/nft/NFTImage'
+import GeneralDetails from '../../components/nft/GeneralDetails'
+import ItemActivity from '../../components/nft/ItemActivity'
+import Purchase from '../../components/nft/Purchase'
+
+const style = {
+  wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
+  container: `container p-6`,
+  topContent: `flex`,
+  nftImgContainer: `flex-1 mr-4`,
+  detailsContainer: `flex-[2] ml-4`,
+}
+
+const Nft = () => {
+  const { provider } = useWeb3()
+  const [selectedNft, setSelectedNft] = useState()
+  const [listings, setListings] = useState([])
+  const router = useRouter()
+
+    console.log("provider");
+    console.log(provider);
+
+const nftModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(provider.getSigner())
+    return sdk.getNFTModule('0x5B862e5cAdbc1136d5deaeD5A25bAe13366c4A39')
+  }, [provider])
+
+  // get all NFTs in the collection ERROR
+  useEffect(() => {
+    if (!nftModule) return;
+    (async () => {
+
+      const nfts = await nftModule.getAll()
+        
+      const selectedNftItem = nfts.find((nft) => nft.id === router.query.nftId)
+      setSelectedNft(selectedNftItem)
+    })()
+  }, [nftModule])
+
+  const marketPlaceModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(provider.getSigner())
+
+    return sdk.getMarketplaceModule(
+      '0x48F715F54bAbF68F3BC696A3e8C638b540aFE124'
+    )
+  }, [provider])
+
+  useEffect(() => {
+    if (!marketPlaceModule) return
+    ;(async () => {
+      setListings(await marketPlaceModule.getAllListings())
+    })()
+  }, [marketPlaceModule])
+
+  return (
+    <div>
+      <Header />
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <div className={style.topContent}>
+            <div className={style.nftImgContainer}>
+              <NFTImage selectedNft={selectedNft} />
+            </div>
+            <div className={style.detailsContainer}>
+              <GeneralDetails selectedNft={selectedNft} />
+              <Purchase
+                isListed={router.query.isListed}
+                selectedNft={selectedNft}
+                listings={listings}
+                marketPlaceModule={marketPlaceModule}
+              />
+            </div>
+          </div>
+          <ItemActivity />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nft
